753076c31d1ec0b524dad14594e6a3c95525aeaf
Daniel RB <daniel@danielrb.dev>
2022-04-23 10:05:21 -0400

    initial commit

---
+++ belayme/common/models.py
@@ -0,0 +1,41 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from common.managers import UserManager
+
+
+class TimestampedModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def update(self, update_dict=None, **kwargs):
+        """ Helper method to update objects """
+        if not update_dict:
+            update_dict = kwargs
+        update_fields = {"updated_on"}
+        for k, v in update_dict.items():
+            setattr(self, k, v)
+            update_fields.add(k)
+        self.save(update_fields=update_fields)
+
+    class Meta:
+        abstract = True
+
+
+# Create your models here.
+class User(AbstractUser, TimestampedModel):
+    email = models.EmailField(unique=True)
+    # START_FEATURE django_social
+    username = None  # disable the AbstractUser.username field
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+    # END_FEATURE django_social
+
+    def __str__(self):
+        return self.email
